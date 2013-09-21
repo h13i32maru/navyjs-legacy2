99484e02365e32b04300f41f79a554ab0143b5fe
h13i32maru <h13i32maru@gmail.com>
2013-09-21 15:42:15 +0900

    リファクタ

--- n_code_widget.h
+++ n_code_widget.h
@@ -2,11 +2,6 @@
 #define N_CODE_WIDGET_H
 
 #include "n_file_tab_editor.h"
-#include <QDir>
-#include <QFileSystemModel>
-#include <QFileSystemWatcher>
-#include <QModelIndex>
-#include <QWidget>
 
 namespace Ui {
 class NCodeWidget;
@@ -18,8 +13,6 @@ class NCodeWidget : public NFileTabEditor
 
 public:
     explicit NCodeWidget(QWidget *parent = 0);
-//    void saveAllFile();
-//    bool saveFile(int tabIndex);
     ~NCodeWidget();
 
 private:
@@ -28,22 +21,6 @@ private:
 protected:
     virtual QWidget* createTabWidget(const QString &filePath);
     virtual QString editedFileContent(QWidget *widget);
-
-//    QList<int> searchTabIndexesByPath(const QString &path, const bool &isDir);
-//    bool isFileContentChanged(int tabIndex);
-
-private slots:
-//    virtual void contextMenu(QPoint point);
-//    void closeFile(int tabIndex);
-//    void newFile();
-//    void newDir();
-//    void deletePath();
-//    void renamePath();
-//    void copyPath();
-//    void updateTabForPathChanged(const QString &oldPath, const QString &newPath);
-//    void updateTabForDropped(QString dropDirPath, QString selectedFilePath);
-//    void updateTabForTextChanged();
-//    void updateTabForPathDeleted(const QString &path, const bool &isDir);
 };
 
 #endif // N_CODE_WIDGET_H
