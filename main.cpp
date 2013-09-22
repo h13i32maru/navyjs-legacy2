df7c20358b747e22e0afe91152412e1fcd41a70e
h13i32maru <h13i32maru@gmail.com>
2013-09-22 11:54:27 +0900

    サイズを最大化するようにした

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "main_window.h"
 #include <QApplication>
+#include <QGuiApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.resize(QGuiApplication::primaryScreen()->availableSize());
     w.show();
 
     return a.exec();
