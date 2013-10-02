72c1b84ae499c1b97b8244690cabfbc37eaaf50e
h13i32maru <h13i32maru@gmail.com>
2013-10-02 09:49:12 +0900

    コメント追記

--- extend/n_tree_widget.cpp
+++ extend/n_tree_widget.cpp
@@ -4,6 +4,10 @@ NTreeWidget::NTreeWidget(QWidget *parent) : QTreeWidget(parent)
 {
 }
 
+/*
+ * 要素がドロップされてツリーの内容が変わったことを
+ * シグナルとして送信するためにオーバーライドしている.
+ */
 void NTreeWidget::dropEvent(QDropEvent *event) {
     QTreeWidget::dropEvent(event);
 
