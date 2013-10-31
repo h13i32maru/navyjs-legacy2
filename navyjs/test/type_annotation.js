6fa8282d6f45b22d0f7f65b985ed2681de73f265
h13i32maru <h13i32maru@gmail.com>
2013-10-31 21:58:42 +0900

    ユニットテスト用にbusterのannotationを追加

--- navyjs/test/type_annotation.js
+++ navyjs/test/type_annotation.js
@@ -1,8 +1,61 @@
 /**
- * @typedef {{
- *   testCase: function,
- *   assert: function,
- *   isFunction: function
- * }}
+ * @typedef {Object}
  */
 buster;
+
+/**
+ * @param {string} name
+ * @param {Object} tests
+ */
+buster.testCase = function(name, tests){};
+
+/**
+ * @param {boolean} actual
+ * @param {string} [message]
+ */
+buster.assert = function(actual, message) {};
+
+/**
+ * @param {boolean} actual
+ * @param {string} [message]
+ */
+buster.assert.isTrue = function(actual, message) {};
+
+/**
+ * @param {boolean} actual
+ * @param {string} [message]
+ */
+buster.assert.isFalse = function(actual, message) {};
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string} [message]
+ */
+buster.assert.equals = function(actual, expected, message) {};
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string} [message]
+ */
+buster.assert.same = function(actual, expected, message) {};
+
+/**
+ * @param {boolean} actual
+ * @param {string} [message]
+ */
+buster.refute = function(actual, message) {};
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string} [message]
+ */
+buster.refute.equals = function(actual, expected, message) {};
+
+/**
+ * @param {*} actual
+ * @param {string} [message]
+ */
+buster.refute.defined = function(actual, message) {};
