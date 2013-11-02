352471c5332efe1049583cd17e0aa40f405ebae8
h13i32maru <h13i32maru@gmail.com>
2013-11-02 19:15:04 +0900

    SlideLeft実装

---
+++ navyjs/src/transition/slide_left.js
@@ -0,0 +1,67 @@
+/**
+ * @class Navy.Transition.SlideLeft
+ */
+Navy.Class('Navy.Transition.SlideLeft', Navy.Transition.Transition, {
+  $static: {
+    initAnimationStyle: false
+  },
+
+  _beforeView: null,
+  _afterView: null,
+
+  initialize: function(beforeView, afterView){
+    this._beforeView = beforeView;
+    this._afterView = afterView;
+
+    if (!this.$class.initAnimationStyle) {
+      this._addAnimationStyle();
+      this.$class.initAnimationStyle = true;
+    }
+
+    var width = Navy.Config.app.size.width;
+    afterView._setRawStyle({webkitAnimation: '0.5s', webkitTransform: 'translateX(-' + width + 'px)'});
+  },
+
+  _addAnimationStyle: function(){
+    var width = Navy.Config.app.size.width;
+    var animIn  = '@-webkit-keyframes slide_left_in  {100% {-webkit-transform: translateX(0)}}';
+    var animOut = '@-webkit-keyframes slide_left_out {100% {-webkit-transform: translateX(-%width%px)}}'.replace('%width%', width);
+    var styleElm = document.createElement('style');
+    styleElm.textContent = animIn + animOut;
+    document.head.appendChild(styleElm);
+  },
+
+  start: function(callback) {
+    if (!this._beforeView) {
+      this._afterView._setRawStyle({webkitTransform: 'none'});
+      callback && callback();
+      return;
+    }
+
+    var cb = function(){
+      this._beforeView.setVisible(false);
+      this._afterView.removeRawEventListener('webkitAnimationEnd', cb);
+      this._afterView._setRawStyle({webkitTransform: 'none', webkitAnimationName: 'none'});
+      callback && callback();
+    }.bind(this);
+
+    this._afterView.addRawEventListener('webkitAnimationEnd', cb);
+    this._afterView._setRawStyle({webkitAnimationName: 'slide_left_in'});
+  },
+
+  back: function(callback) {
+    if (!this._beforeView) {
+      callback && callback();
+      return;
+    }
+
+    var cb = function(){
+      this._afterView.removeRawEventListener('webkitAnimationEnd', cb);
+      callback && callback();
+    }.bind(this);
+
+    this._beforeView.setVisible(true);
+    this._afterView.addRawEventListener('webkitAnimationEnd', cb);
+    this._afterView._setRawStyle({webkitAnimationName: 'slide_left_out'});
+  }
+});
