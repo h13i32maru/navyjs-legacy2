43e320d25ce6d8049453f0501a9da0c14f8a44c9
h13i32maru <h13i32maru@gmail.com>
2014-01-13 16:02:56 +0900

    nativeとのブリッジの初期化処理を分離.

--- creator/src/template/contents/creator/creator_page.js
+++ creator/src/template/contents/creator/creator_page.js
@@ -25,19 +25,6 @@ Navy.Class('CreatorPage', Navy.Page, {
     Navy.Resource.loadLayout(this._layout.extra.contentLayoutFile, function(layout){
       Native.setViewsFromJS(JSON.stringify(layout));
     });
-
-    Native.changedViewsOrderToJS.connect(this._updateViewsOrderFromNative.bind(this));
-    Native.changedSelectedViewToJS.connect(this._selectViewsFromNative.bind(this));
-    Native.changedViewPropertyToJS.connect(this._updateSelectedViewLayoutFromNative.bind(this));
-    Native.addViewToJS.connect(this._addViewFromNative.bind(this));
-    Native.deleteSelectedViewsToJS.connect(this._deleteSelectedViewsFromNative.bind(this));
-    Native.setScreenToJS.connect(this._setScreenFromNative.bind(this));
-    Native.setScreenEnableToJS.connect(this._setScreenEnableFromNative.bind(this));
-    Native.unselectAllViewsToJS.connect(this._unselectAllViewsFromNative.bind(this));
-    Native.alignSelectedViewsToJS.connect(this._alignSelectedViewsFromNative.bind(this));
-    Native.arrangeSelectedViewsToJS.connect(this._arrangeSelectedViewsFromNative.bind(this));
-    Native.groupingViewsToJS.connect(this._execGroupingViewsFromNative.bind(this));
-    Native.ungroupingViewsToJS.connect(this._execUngroupingViewsFromNative.bind(this));
   },
 
   onResumeAfter: function($super, ev) {
--- creator/src/template/contents/creator/include/native_bridge.js
+++ creator/src/template/contents/creator/include/native_bridge.js
@@ -2,6 +2,25 @@
  * @typedef {Object} Include.NativeBridge
  */
 Navy.Class.instance('Include.NativeBridge', Include.Include, {
+  initialize: function($super, targetObject) {
+    $super(targetObject);
+
+    (function(){
+      Native.changedViewsOrderToJS.connect(this._updateViewsOrderFromNative.bind(this));
+      Native.changedSelectedViewToJS.connect(this._selectViewsFromNative.bind(this));
+      Native.changedViewPropertyToJS.connect(this._updateSelectedViewLayoutFromNative.bind(this));
+      Native.addViewToJS.connect(this._addViewFromNative.bind(this));
+      Native.deleteSelectedViewsToJS.connect(this._deleteSelectedViewsFromNative.bind(this));
+      Native.setScreenToJS.connect(this._setScreenFromNative.bind(this));
+      Native.setScreenEnableToJS.connect(this._setScreenEnableFromNative.bind(this));
+      Native.unselectAllViewsToJS.connect(this._unselectAllViewsFromNative.bind(this));
+      Native.alignSelectedViewsToJS.connect(this._alignSelectedViewsFromNative.bind(this));
+      Native.arrangeSelectedViewsToJS.connect(this._arrangeSelectedViewsFromNative.bind(this));
+      Native.groupingViewsToJS.connect(this._execGroupingViewsFromNative.bind(this));
+      Native.ungroupingViewsToJS.connect(this._execUngroupingViewsFromNative.bind(this));
+    }).apply(targetObject);
+  },
+
   _addViewFromNative: function(viewId, viewClass, layout) {
     this._createNewView(viewId, viewClass, layout);
   },
