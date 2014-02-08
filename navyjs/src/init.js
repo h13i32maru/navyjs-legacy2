1a610d20676203e0b20a18cbb2bfc3d15a0031fc
h13i32maru <h13i32maru@gmail.com>
2014-02-08 12:25:17 +0900

    不具合修正.

--- navyjs/src/init.js
+++ navyjs/src/init.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', function(){
       progressElement.style.width = (100 * progress / total) + '%';
     },
     onComplete: function() {
-      var progressElement = document.querySelector('#web_installer_inner_progress');
+      var progressElement = document.querySelector('#web_installer_progress');
       progressElement.parentElement.removeChild(progressElement);
       Navy.App.initialize();
     },
