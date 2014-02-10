ccaffc6d45cca3943a3babefd5bc031521d73e20
h13i32maru <h13i32maru@gmail.com>
2014-02-10 18:00:26 +0900

    Classのspecを整理.

--- navyjs/spec/src/lib/class_spec.js
+++ navyjs/spec/src/lib/class_spec.js
@@ -1,11 +1,17 @@
 describe("Navy.Class is class base OOP:", function() {
   /**
-   * @class ClassA
+   * @class Spec.ClassA
    */
-  Navy.Class('ClassA', {
+  Navy.Class('Spec.ClassA', {
+    $static: {
+      say: function(){
+        return 'ClassA.say';
+      }
+    },
     arg1: null,
     arg2: null,
     obj: null,
+    x: 0,
 
     initialize: function(arg1, arg2) {
       this.arg1 = arg1;
@@ -16,80 +22,26 @@ describe("Navy.Class is class base OOP:", function() {
 
     say: function() {
       return 'ClassA#say';
+    },
+
+    getX: function() {
+      return this.x;
     }
   });
 
   /**
-   * @class ClassB
+   * @class Spec.ClassB
    */
-  Navy.Class('ClassB', {
+  Navy.Class('Spec.ClassB', {
     say: function() {
       return 'ClassB#sayB';
     }
   });
 
-  it("create instance.", function() {
-    var a = new ClassA();
-    var b = new ClassB();
-
-    expect(a instanceof ClassA).toBeTruthy();
-    expect(b instanceof ClassB).toBeTruthy();
-  });
-
-  it('initialize function is called when use "new"', function() {
-    var arg1 = 100;
-    var arg2 = [-1, -2];
-    var a = new ClassA(arg1, arg2);
-
-    expect(a.arg1).toBe(arg1);
-    expect(a.arg2).toBe(arg2);
-  });
-
-  it('can call method', function() {
-    var a = new ClassA();
-    expect(a.say()).toBe('ClassA#say');
-  });
-
-  it('each instance property are independence.', function() {
-    var a1 = new ClassA();
-    var a2 = new ClassA();
-
-    a1.obj['food'] = 'rice';
-
-    expect(a1.obj['food']).toBe('rice');
-    expect(a2.obj['food']).toBeUndefined();
-    expect(a1.obj).not.toBe(a2.obj);
-  });
-});
-
-describe('Navy.Class special property', function(){
   /**
-   * @class Foo.Bar.ClassA
+   * @class Spec.ClassC
    */
-  Navy.Class('Foo.Bar.ClassA', {});
-
-  it('can access constructor', function() {
-    var a = new Foo.Bar.ClassA();
-
-    expect(a.$class).toBe(Foo.Bar.ClassA);
-  });
-
-  it('can access class name', function() {
-    var a = new Foo.Bar.ClassA();
-
-    expect(a.$className).toBe('Foo.Bar.ClassA');
-  });
-
-  it('can access constructor function name', function() {
-    expect(Foo.Bar.ClassA.name).toBe('Foo$Bar$ClassA');
-  });
-});
-
-describe('Navy.Class static property and method', function() {
-  /**
-   * @class ClassC
-   */
-  Navy.Class('ClassC', {
+  Navy.Class('Spec.ClassC', {
     $static: {
       value: {x: 10},
 
@@ -103,205 +55,226 @@ describe('Navy.Class static property and method', function() {
     }
   });
 
-  it('has static property', function() {
-    var a1 = new ClassC();
-    var a2 = new ClassC();
-
-    expect(a1.$class.x).toBe(ClassC.x);
-    expect(a2.$class.x).toBe(ClassC.x);
-  });
-
-  it('call static method', function() {
-    var a1 = new ClassC();
-    var a2 = new ClassC();
-
-    ClassC.setValue(100);
-    expect(a1.$class.getValue()).toBe(ClassC.getValue());
-    expect(a2.$class.getValue()).toBe(ClassC.getValue());
-  });
-});
-
-describe('Navy.Class exception', function() {
-  it('throw property error when property is Object', function() {
-    try {
-      Navy.Class('ClassA', {
-        obj: {}
-      });
-    } catch(e) {
-      expect(true).toBeTruthy();
-      return;
-    }
-
-    expect(true).toBeFalsy();
-  });
-
-  it('throw argument error when argument number is not enough', function() {
-    try {
-      Navy.Class();
-    } catch(e) {
-      expect(true).toBeTruthy();
-    }
-
-    try {
-      Navy.Class('ClassA');
-    } catch(e) {
-      expect(true).toBeTruthy();
-      return;
-    }
-
-    expect(true).toBeFalsy();
-  });
-
-  it('throws not function exception.', function(){
-    var exception = null;
-    try {
-      Navy.Class._wrapFunction({doSomething: 'not function'}, 'doSomething', function(){});
-    } catch(e) {
-      exception = e;
-    }
-
-    expect(exception).not.toBeNull();
-  });
-
-  it('throws already defined exception.', function(){
-    var exception = null;
-
-    Navy.Class('ClassA5', {});
-    try {
-      Navy.Class('ClassA5', {});
-    } catch(e) {
-      exception = e;
-    }
-
-    expect(exception).not.toBeNull();
-  });
-
-  it('throws object property exception.', function(){
-    var exception = null;
-
-    try {
-      Navy.Class('InvalidClassA6', {
-        prop: {}
-      });
-    } catch(e) {
-      exception = e;
-    }
-
-    expect(exception).not.toBeNull();
-  });
-});
-
-describe('Navy.Class extend', function() {
   /**
-   * @class classAA
+   * @class Spec.ClassAA
    */
-  Navy.Class('ClassAA', {
-    x: 0,
-    initialize: function(arg) {
-      this.x = arg;
+  Navy.Class('Spec.ClassAA', Spec.ClassA, {
+    $static: {
+      say: function($super) {
+        return $super() + ':ClassAA';
+      }
     },
 
-    getX: function() {
-      return this.x;
+    initialize: function($super, arg1, arg2) {
+      $super(arg1, arg2);
     },
 
-    say: function() {
-      return 'ClassA';
-    }
-  });
-
-  /**
-   * @class classBB
-   */
-  Navy.Class('ClassBB', ClassAA, {
-    initialize: function($super, arg) {
-      $super(arg);
-    },
-
+    /**
+     *
+     * @param [$super]
+     * @returns {string}
+     */
     say: function($super) {
       var result = $super();
       return result + ':ClassB';
     }
   });
 
-  it('extend class', function() {
-    var b = new ClassBB();
-
-    expect(b instanceof ClassBB).toBeTruthy();
-    expect(b instanceof ClassAA).toBeTruthy();
-  });
-
-  it('can override initialize method', function() {
-    var value = {num: 10};
-    var b = new ClassBB(value);
-
-    expect(b.x).toBe(value);
-  });
-
-  it('can extend method', function() {
-    var value = {num: 10};
-    var b = new ClassBB(value);
-
-    expect(b.getX()).toBe(value);
-  });
-
-  it('can override method', function() {
-    var b = new ClassBB();
-
-    expect(b.say()).toBe('ClassA:ClassB');
-  });
-});
-
-describe('Navy.Class extend static', function() {
   /**
-   * @class ClassAAA
+   * @typedef {Object} Spec.InstanceA
    */
-  Navy.Class('ClassAAA', {
-    $static: {
-      say: function() {
-        return 'ClassA';
-      }
-    }
-  });
-
-  Navy.Class('ClassBBB', ClassAAA, {
-    $static: {
-      say: function($super) {
-        return $super() + ':ClassB';
-      }
-    }
-  });
-
-  it('can override method', function() {
-    expect(ClassBBB.say()).toBe('ClassA:ClassB');
-  });
-});
-
-describe('Navy.Class.instance create singleton instance:', function(){
-  /**
-   * @typedef {Object} Instance1
-   */
-  Navy.Class.instance('Instance1', {
+  Navy.Class.instance('Spec.InstanceA', {
     getName: function() {
-      return 'Instance1';
+      return 'InstanceA';
     }
   });
 
   /**
-   * @class Class4
+   * @class Spec.ClassD
    */
-  Navy.Class('Class4', Instance1, {
+  Navy.Class('Spec.ClassD', Spec.InstanceA, {
     getName: function($super) {
-      return $super() + ':Class4';
+      return $super() + ':ClassD';
     }
   });
 
-  it('can be used like singleton.', function(){
-    expect(Instance1.getName()).toBe('Instance1');
+  describe('Simple usage:', function(){
+    it('created constructor function.', function(){
+      expect(typeof Spec.ClassA).toBe('function');
+      expect(typeof Spec.ClassB).toBe('function');
+    });
+
+    it("create instance.", function() {
+      var a = new Spec.ClassA();
+      var b = new Spec.ClassB();
+
+      expect(a instanceof Spec.ClassA).toBeTruthy();
+      expect(b instanceof Spec.ClassB).toBeTruthy();
+    });
+
+    it('initialize function is called when use "new"', function() {
+      var arg1 = 100;
+      var arg2 = [-1, -2];
+      var a = new Spec.ClassA(arg1, arg2);
+
+      expect(a.arg1).toBe(arg1);
+      expect(a.arg2).toBe(arg2);
+    });
+
+    it('can call method', function() {
+      var a = new Spec.ClassA();
+      expect(a.say()).toBe('ClassA#say');
+    });
+
+    it('each instance property are independence.', function() {
+      var a1 = new Spec.ClassA();
+      var a2 = new Spec.ClassA();
+
+      a1.obj['food'] = 'rice';
+
+      expect(a1.obj['food']).toBe('rice');
+      expect(a2.obj['food']).toBeUndefined();
+      expect(a1.obj).not.toBe(a2.obj);
+    });
   });
 
-  it('can extend singleton instance', function(){
-    var obj = new Class4();
-    expect(obj.getName()).toBe('Instance1:Class4');
+  describe('Special property:', function(){
+    it('can access constructor', function() {
+      var a = new Spec.ClassA();
+
+      expect(a.$class).toBe(Spec.ClassA);
+    });
+
+    it('can access class name', function() {
+      var a = new Spec.ClassA();
+
+      expect(a.$className).toBe('Spec.ClassA');
+    });
+
+    it('can access constructor function name', function() {
+      expect(Spec.ClassA.name).toBe('Spec$ClassA');
+    });
+  });
+
+  describe('Static property and method:', function() {
+    it('has static property', function() {
+      var a1 = new Spec.ClassC();
+      var a2 = new Spec.ClassC();
+
+      expect(a1.$class.x).toBe(Spec.ClassC.x);
+      expect(a2.$class.x).toBe(Spec.ClassC.x);
+    });
+
+    it('call static method', function() {
+      var a1 = new Spec.ClassC();
+      var a2 = new Spec.ClassC();
+
+      Spec.ClassC.setValue(100);
+      expect(a1.$class.getValue()).toBe(Spec.ClassC.getValue());
+      expect(a2.$class.getValue()).toBe(Spec.ClassC.getValue());
+    });
+  });
+
+  describe('Extend class:', function() {
+
+    it('extend class', function() {
+      var a = new Spec.ClassAA();
+
+      expect(a instanceof Spec.ClassAA).toBeTruthy();
+      expect(a instanceof Spec.ClassA).toBeTruthy();
+    });
+
+    it('can override initialize method', function() {
+      var a = new Spec.ClassAA(10, 20);
+
+      expect(a.arg1).toBe(10);
+      expect(a.arg2).toBe(20);
+    });
+
+    it('can extend method', function() {
+      var a = new Spec.ClassAA(10, 20);
+
+      expect(a.getX()).toBe(0);
+    });
+
+    it('can override method', function() {
+      var a = new Spec.ClassAA();
+
+      expect(a.say()).toBe('ClassA#say:ClassB');
+    });
+  });
+
+  describe('Extend class that has static method:', function() {
+    it('can override method', function() {
+      expect(Spec.ClassAA.say()).toBe('ClassA.say:ClassAA');
+    });
+  });
+
+  describe('Create singleton instance:', function(){
+
+    it('can be used like singleton.', function(){
+      expect(Spec.InstanceA.getName()).toBe('InstanceA');
+    });
+
+    it('can extend singleton instance', function(){
+      var obj = new Spec.ClassD();
+      expect(obj.getName()).toBe('InstanceA:ClassD');
+    });
+  });
+
+  describe('Error occurs:', function() {
+    it('throw property error when property is Object', function() {
+      var exception;
+      try {
+        Navy.Class('Spec.ClassX', {
+          obj: {}
+        });
+      } catch(e) {
+        exception = e;
+      }
+
+      expect(exception.message).toContain('object property must be primitive type.');
+    });
+
+    it('throw argument error when argument number is not enough', function() {
+      var exception;
+      try {
+        Navy.Class();
+      } catch(e) {
+        exception = e;
+      }
+      expect(exception.message).toContain('arguments of Navy.Class is 2 or 3.');
+
+      try {
+        Navy.Class('ClassA');
+      } catch(e) {
+        exception = e;
+      }
+
+      expect(exception.message).toContain('arguments of Navy.Class is 2 or 3.');
+    });
+
+    it('throws not function exception.', function(){
+      var exception;
+      try {
+        Navy.Class._wrapFunction({doSomething: 'not function'}, 'doSomething', function(){});
+      } catch(e) {
+        exception = e;
+      }
+
+      expect(exception.message).toContain('override method must be function');
+    });
+
+    it('throws already defined exception.', function(){
+      var exception;
+
+      try {
+        Navy.Class('Spec.ClassA', {});
+      } catch(e) {
+        exception = e;
+      }
+
+      expect(exception.message).toContain('already defined this className');
+    });
   });
 });
