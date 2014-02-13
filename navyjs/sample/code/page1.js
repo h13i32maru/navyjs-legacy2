182bc52429c061d4666dc59ace98ec4c01f8f952
h13i32maru <h13i32maru@gmail.com>
2014-02-13 21:46:20 +0900

    Tabのサンプルを組み込んだ.

--- navyjs/sample/code/page1.js
+++ navyjs/sample/code/page1.js
@@ -2,12 +2,17 @@ Navy.Class('Page1', Navy.Page, {
   onCreate: function($super, ev) {
     $super(ev);
 
-    var listView = this.findViewById('list_view');
     var items = [];
     for (var i = 0; i < 30; i++) {
       items.push({id: i, name: 'name' + i});
     }
 
+    var listView = this.findViewById('tab1.list_view');
+    listView.setItems(items, function(item, viewGroup){
+      viewGroup.findViewById('name').setText(item.name.toUpperCase());
+    });
+
+    var listView = this.findViewById('tab3.list_view');
     listView.setItems(items, function(item, viewGroup){
       viewGroup.findViewById('name').setText(item.name.toUpperCase());
     });
