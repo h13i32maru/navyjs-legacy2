6afc89dbe6d01456d5969e858827618393251f92
h13i32maru <h13i32maru@gmail.com>
2014-02-16 22:11:32 +0900

    jasmine2.0ようにとりあえずannotation作った.

---
+++ navyjs/spec/annotation.js
@@ -0,0 +1,15 @@
+/*
+ * annotation for jasmine.
+ * jasmine本家で対応されたらこのファイルは削除する.
+ */
+
+/**
+ * @param {string} description
+ * @param {function} specDefinitions
+ */
+var describe = function(description, specDefinitions){};
+
+var expect = {};
+expect.toBe = function(actual){};
+expect.toEqual = function(actual){};
+expect.toContain = function(actual){};
