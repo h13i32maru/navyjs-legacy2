0cfde5f7822f846b0796abe0d51cf18363f6c003
h13i32maru <h13i32maru@gmail.com>
2014-02-16 15:44:19 +0900

    styleをちょっと変更.

--- creator/src/extend/n_text_list_selector.cpp
+++ creator/src/extend/n_text_list_selector.cpp
@@ -9,8 +9,6 @@ NTextListSelector::NTextListSelector(QWidget *parent) : QPushButton(parent) {
 
     this->setFlat(true);
     this->setStyleSheet("QPushButton { \
-                        background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(230, 230, 230, 255), stop:1 rgba(200, 200, 200, 255)); \
-                        border: 1px solid #aaaaaa; \
                         text-align: left;\
                       }");
 }
